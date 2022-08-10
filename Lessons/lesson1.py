1a7ebe9a4ff0f8277452949be0efbfac8e510512
Vitala35 <vitalik.osepchuk@gmail.com>
2022-08-10 20:50:27 +0300

    lesson1

---
+++ Lessons/lesson1.py
@@ -0,0 +1,19 @@
+# name = 'Andriy'
+# age = 12
+#
+# print(name, age)  # print value in terminal
+
+
+# a = 2
+# b = 3
+#
+# print(a * b)  # print result plural
+
+
+age = 18
+name = 'Tara'
+isMaried = True
+PI = 3.14
+
+print(type(PI))
+print(type(2))
