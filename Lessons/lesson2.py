04a05ae0fa0219c244f44a280c1937005c1c8adb
Vitala35 <vitalik.osepchuk@gmail.com>
2022-08-14 14:25:40 +0300

    Math operations

---
+++ Lessons/lesson2.py
@@ -0,0 +1,17 @@
+# Math operations
+# print(2 + 2)
+# print(3 - 2)
+# print(3 * 5)
+# print(7 / 2)
+# print(3 ** 2)
+# print(pow(3, 2))
+# print(7 % 2)
+# print(7 // 2)
+
+
+# Funksion round
+# print(round(2.123))
+# print(round(2.123, 2))
+# print(round(2.49))
+# print(round(2.5))
+# print(round(2.50001))
---
+++ Practics/practic1.py
@@ -0,0 +1,19 @@
+# print(17 + 2)
+# print(17 - 2)
+# print(17 * 2)
+# print(17 / 2)
+# print(17 ** 2)
+# print(17 // 2)
+# print(17 % 2)
+# print(pow(17, 2))
+
+
+# print(round(2.001234, 3))
+
+
+# print(round(2.565))
+
+
+first_number = float(input('enter first value:'))
+second_number = float(input('enter first value:'))
+print(round(first_number * second_number, 2))
