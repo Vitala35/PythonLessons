e0da658884e57882874557b2b31e090ad26ec092
Vitala35 <vitalik.osepchuk@gmail.com>
2022-08-16 20:26:01 +0300

    lesson3

---
+++ Lessons/lesson3.py
@@ -0,0 +1,16 @@
+# a = input('Enter value a:')
+# b = input('Enter value b:')
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+c = int(input('Enter value a:'))
+d = int(input('Enter value b:'))
+
+# print(c == 3 and d == 4)
+# print(c == 5 or d == 6)
+# print(not c > d)
