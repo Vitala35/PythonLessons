6af993456bb805ce13cb9e94a86b26baea25de93
Vitala35 <vitalik.osepchuk@gmail.com>
2022-08-19 20:04:12 +0300

    lesson3

--- Lessons/lesson3.py
+++ Lessons/lesson3.py
@@ -8,9 +8,21 @@
 # print(a >= b)
 # print(a <= b)
 
-c = int(input('Enter value a:'))
-d = int(input('Enter value b:'))
+# c = int(input('Enter value a:'))
+# d = int(input('Enter value b:'))
 
 # print(c == 3 and d == 4)
 # print(c == 5 or d == 6)
 # print(not c > d)
+#1
+privet = ['Vitalii', 'Nazar', 'David', 'Eduard']
+for i in privet:
+    print(i)
+#2
+colors = ['green', 'yellow', 'red']
+print('green' in colors)
+
+#3
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+print(a is b)
