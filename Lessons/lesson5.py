561316a86918acbbfd568d2b83df059bd0b6c793
Vitala35 <vitalik.osepchuk@gmail.com>
2022-09-09 20:30:28 +0300

    Practic lesson

---
+++ Lessons/lesson5.py
@@ -0,0 +1,58 @@
+# name = ['Andriy', 'Sergey', 'Vika', 'Natasha']
+# for i in name:
+#     print(i)
+#
+# city = ['Kiev', 'Madrid', 'Lviv', 'Chernivtsi']
+# number = 1
+# for i in city:
+#     print(i, '-', number)
+#     number += 1
+
+
+# for i in range(5):
+#     print(i)
+#
+# for i in range(2, 11):
+#     print(i)
+
+# for i in range(0, 101, 2):
+#     print(i)
+#
+# name = ['Andriy', 'Sergey', 'Vika', 'Anna', 'Taras', 'Marina', 'Ivan']
+# number = 1
+# for i in name:
+#     print(number, ')', i)
+#     number += 1
+
+# numbers = [1, 2, 3, 4, 5, 10, 15, 20, 60, 90]
+# for i in numbers:
+#     if i > 10:
+#         print(i * 0.1)
+#     if i < 10:
+#         print(i * 100)
+
+# country = ['Chernivtsi', 'Lviv', 'Kiew', 'Herson']
+# number = 1
+# for i in country:
+#     print(number, ')', i)
+#     number += 1
+
+# name = ['Nazar', 'Taras', 'Ivan', 'Andriy', 'Dan']
+# for i in name:
+#     if len(i) > 4:
+#         print(f'{i} this name is realise')
+#     else:
+#         print('Any numbers')
+
+
+# a = int(input('Please enter a:'))
+# b = int(input('Please enter b:'))
+# lst = []
+# for i in range(a, b + 1):
+#     lst.append(i)
+# naw_element = 0
+# for i in lst:
+#     naw_element += i
+#
+# print(naw_element / len(lst))
+
